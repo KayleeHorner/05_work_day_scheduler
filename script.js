1097895f872a444ff08101279e2515c8cb1e8eec
Kaylee Horner <kayleeahorner@gmail.com>
2020-10-14 01:54:06 -0500

    added local storage element

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
-
-
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar -- FIGURE OUT MOMENT
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
 // WHEN I view the time blocks for that day
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future -- figure out moment, then use if else to color code 
 // WHEN I click into a time block
-// THEN I can enter an event - on click, entre text
+// THEN I can enter an event - on click, enter text
 // WHEN I click the save button for that time block
 // THEN the text for that event is saved in local storage - use font awesome icon, save to local storage
 // WHEN I refresh the page
 // // THEN the saved events persist - local storage persists, check the form assignment
 
 $(document).ready(function() {
-var currentDay = document.querySelector("#current-day");
-var m = moment();
-var timeBlocks = moment().hours();
 
+var currentDay = document.querySelector("#current-day");
+
+// Adds save button to page 
+$(".saveBtn").html("<i></i>").addClass("far fa-save");
+
+// Current date on top of page
 function renderDate() {
-currentDay = moment().format("MMM Do YY");
-$("#current-day").text(currentDay);
+    currentDay = moment().format("MMMM Do, YYYY");
+    $("#current-day").text(currentDay);
 }
+
 renderDate()
 
-// Need to align these to the right and put each on a new line/in their on little box
-$(".time-block").children().css("background", "lightgray");
-$(".time-block").children().html("<i></i>").addClass("far fa-save");
+// Change color based on time
+var currentTime = moment().format("HH");
+$(".hour").addClass(".present");
+for (var i = 8; i < 17 ; i++) {
+    if (currentTime < 1) {
+        $(".hour").addClass(".future");
+    } else if (currentTime > 1) {
+        $(".hour").addClass(".past");
+       
+    }
+}
+
+// Saves events to local storage on click 
+
+function saveButton() {
+   
+    $(".event-input").each(function() {
+    var events = $(this).attr("id");
+    $(this).val(localStorage.getItem(events));
+    });
+
+    $(".saveBtn").on("click", function() {
+        var data = $(this).siblings(".event-input").val();
+        var dataId = $(this).siblings(".event-input").attr("id");
+
+        localStorage.setItem(dataId, data);
+       
+    })
+}
+
+saveButton()
+
+
+// function determineHour() {
+//     if (timeBlocks = )
+// }
+
 })
+
+
+
+
